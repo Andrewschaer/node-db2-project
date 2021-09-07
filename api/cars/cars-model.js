009d0d33de4f802d5b1f5d60306d8fcc4f0ee444
Andrewschaer <andrewschaer@github.com>
2021-09-07 18:26:51 -0400

    Async error fixed in cars-model create function

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -15,11 +15,11 @@ const getByVin = (vin) => {
   return db('cars').where('vin', vin).first();
 };
 
-async function create = (newCar) => {
+async function create (newCar) {
   // insert into cars (vin, make, model, mileage)
   // values ('11111111111111111', 'Ford', 'Bronco', 100000);
   const [id] = await db('cars').insert(newCar);
   return getById(id);
-};
+}
 
 module.exports = { getAll, getById, getByVin, create };
