4596fb5a60c3262526cb358cb87065ba13731a99
Andrewschaer <andrewschaer@github.com>
2021-09-07 18:40:01 -0400

    migration file built to create cars table w/ provided schema + migration ran successfully to create dealer db w/ cars table schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+exports.up = async function (knex) {
+  await knex.schema.createTable('cars', table => {
+    table.increments(); //creates a primary key called id of auto inc integers
+    table.string('vin').unique().notNullable();
+    table.string('make').notNullable();
+    table.string('model').notNullable();
+    table.float('mileage').notNullable();
+    table.string('title');
+    table.string('transmission');
+  });
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars')
 };
